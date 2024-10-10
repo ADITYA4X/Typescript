05abffdf723c5c5ba9542346bf4ac6666dea0941
aditya <adityakunghadkar94.com>
2024-10-11 05:23:02 +0530

    Basic Typescript

---
+++ src/functions.ts
@@ -0,0 +1,27 @@
+// function add(n1: number, n2: number): string {
+//   return n1.toString() + n2.toString();
+// }
+
+// function printResult(num: string): undefined {
+//   console.log("Result: " + num);
+//   return;
+// }
+
+// function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+//   const result = n1 + n2;
+//   cb(result);
+// }
+
+// printResult(add(10, 10));
+
+// // let combineValues: Function;
+// let combineValues: (a: number, b: number) => string;
+
+// combineValues = add;
+// // combineValues = printResult;
+
+// console.log(combineValues(8, 8));
+
+// addAndHandle(10, 20, (result) => {
+//   console.log(result);
+// });
---
+++ src/obj-arr-enum.ts
@@ -0,0 +1,35 @@
+// // const person: {
+// //   name: string;
+// //   age: number;
+// //   hobbies: any[];
+// //   role: [number, string];
+// // } = {
+
+// enum Role {
+//   Admin,
+//   Read_only,
+//   Author,
+// }
+
+// const person = {
+//   name: "Aditya",
+//   age: 27,
+//   hobbies: ["sports", "Cooking"],
+//   //   role: [2, "author"],
+//   role: Role.Admin,
+// };
+
+// // person.role.push("admin");
+// //  person.role[1] = 10;
+
+// console.log(person.role);
+
+// console.log(person.name);
+
+// for (const hobby of person.hobbies) {
+//   console.log(hobby.toUpperCase());
+// }
+
+// if (person.role === 0) {
+//   console.log(" is Admin");
+// }
